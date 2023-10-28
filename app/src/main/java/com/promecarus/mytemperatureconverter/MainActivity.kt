676484f5a68c42ba3b632c58875213c6e2f831c4
promecarus <haikalslipi@gmail.com>
2023-10-28 14:38:22 +0700

    part 01

--- app/src/main/java/com/promecarus/mytemperatureconverter/MainActivity.kt
+++ app/src/main/java/com/promecarus/mytemperatureconverter/MainActivity.kt
@@ -3,13 +3,24 @@ package com.promecarus.mytemperatureconverter
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.unit.dp
 import com.promecarus.mytemperatureconverter.ui.theme.MyTemperatureConverterTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,28 +30,39 @@ class MainActivity : ComponentActivity() {
             MyTemperatureConverterTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
+                    modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    StatefulTemperatureInput()
                 }
             }
         }
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    MyTemperatureConverterTheme {
-        Greeting("Android")
+fun StatefulTemperatureInput(
+    modifier: Modifier = Modifier,
+) {
+    var input by rememberSaveable { mutableStateOf("") }
+    var output by rememberSaveable { mutableStateOf("") }
+    Column(modifier.padding(16.dp)) {
+        Text(
+            text = stringResource(R.string.stateful_converter),
+            style = MaterialTheme.typography.headlineSmall
+        )
+        OutlinedTextField(
+            value = input,
+            label = { Text(stringResource(R.string.enter_celsius)) },
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+            onValueChange = {
+                input = it
+                output = convertToFahrenheit(input)
+            },
+        )
+        Text(stringResource(R.string.temperature_fahrenheit, output))
     }
 }
+
+private fun convertToFahrenheit(celsius: String) =
+    celsius.toDoubleOrNull()?.let { (it * 9 / 5) + 32 }.toString()
